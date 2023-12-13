f4d79f5dc2aafe3c3e35053ab9f4f5358e7f96fc
Emilio Martin <emiunrn@gmail.com>
2023-12-13 16:01:53 -0300

    udp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:isolate';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
   runApp(const MainApp());
@@ -20,14 +21,12 @@ class _MainAppState extends State<MainApp> {
   bool hasConnection = false;
   Isolate? threadListener;
   Isolate? threadSender;
+  int thread = 0;
 
   @override
   void initState() {
     super.initState();
     final port = ReceivePort();
-
-    // new listener thread
-    createBackgroundThread(threadListener, _backgroundListenerTask, port);
 
     // listen messages in the port and change state to display them
     port.listen((message) {
@@ -40,6 +39,9 @@ class _MainAppState extends State<MainApp> {
     // If no messages has been sent in the past 5 seconds try to reconnect
     Timer.periodic(const Duration(seconds: 5), (timer) {
       if (!hasConnection) {
+        setState(() {
+          thread++;
+        });
         createBackgroundThread(threadListener, _backgroundListenerTask, port);
       } else {
         setState(() {
@@ -57,19 +59,30 @@ class _MainAppState extends State<MainApp> {
     thread = await Isolate.spawn(task, port.sendPort);
   }
 
-  static void _backgroundListenerTask(SendPort port) {
-    final channel = WebSocketChannel.connect(Uri.parse('ws://10.0.2.2:8888'));
-    StreamSubscription sub = channel.stream.listen((message) {
-      port.send(message);
+  static void _backgroundListenerTask(SendPort port) async {
+    final serverAddress = (await InternetAddress.lookup('10.0.2.2')).first;
+    final clientSocket = await RawDatagramSocket.bind(
+        serverAddress.type == InternetAddressType.IPv6
+            ? InternetAddress.anyIPv6
+            : InternetAddress.anyIPv4,
+        4444);
+    final ntpQuery = Uint8List(48);
+    ntpQuery[0] = 0x23; // See RFC 5905 7.3
+
+    // starts connection
+    clientSocket.send(ntpQuery, serverAddress, 8888);
+
+    clientSocket.listen((event) {
+      if (event == RawSocketEvent.read) {
+        final datagram = clientSocket.receive();
+        if (datagram != null) {
+          port.send(String.fromCharCodes(datagram.data));
+        }
+      }
     });
   }
 
-  static void _backgroundSenderTask(SendPort port) {
-    final channel = WebSocketChannel.connect(Uri.parse('ws://10.0.2.2:8888'));
-    StreamSubscription sub = channel.stream.listen((message) {
-      port.send(message);
-    });
-  }
+  static void _backgroundSenderTask(SendPort port) {}
 
   @override
   void dispose() {
